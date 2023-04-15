1d30a96e091c462514f94e070ac1b568635ecbeb
Yuri Ramos <yuri.pramos@gmail.com>
2023-04-15 14:22:36 -0300

    Modify `deleteFile` on api/services

--- api/src/services/files/files.ts
+++ api/src/services/files/files.ts
@@ -26,25 +26,35 @@ export const updateFile: MutationResolvers['updateFile'] = ({ id, input }) => {
   })
 }
 
+const isValidURL = (string: string) => {
+  try {
+    // eslint-disable-next-line no-new
+    new URL(string)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
 export const deleteFile: MutationResolvers['deleteFile'] = async ({ id }) => {
   const client = Filestack.init(process.env.REDWOOD_ENV_FILESTACK_API_KEY)
 
   const file = await db.file.findUnique({ where: { id } })
 
-  // The `security.handle` is the unique part of the Filestack file's url.
-  const handle = file.path.split('/').pop()
+  //unique part of the Filestack file's url.
+  if (isValidURL(file.path)) {
+    const handle = file.path.split('/').pop()
+    const security = Filestack.getSecurity(
+      {
+        expiry: new Date().getTime() + 5 * 60 * 1000,
+        handle,
+        call: ['remove'],
+      },
+      process.env.REDWOOD_ENV_FILESTACK_SECRET
+    )
 
-  const security = Filestack.getSecurity(
-    {
-      // We set `expiry` at `now() + 5 minutes`.
-      expiry: new Date().getTime() + 5 * 60 * 1000,
-      handle,
-      call: ['remove'],
-    },
-    process.env.REDWOOD_ENV_FILESTACK_SECRET
-  )
-
-  await client.remove(handle, security)
+    await client.remove(handle, security)
+  }
 
   return db.file.delete({
     where: { id },
